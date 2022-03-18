b6202650b6da2d12713201b65c140f4061b96ca7
Vipul Aggarwal <vipul.agg99@gmail.com>
2022-03-18 13:33:47 +0530

    First Commit

---
+++ Artisto/src/main/java/com/example/demo/ArtistoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtistoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtistoApplication.class, args);
+	}
+
+}
